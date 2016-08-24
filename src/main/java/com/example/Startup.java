189c7bfac71bfe202f5b62a08d4f299ec456dbbe
Alex Stockinger <alex@it-stockinger.de>
2016-08-24 08:51:51 +0200

    Trying paho client 1.1.1-SNAPSHOT

--- src/main/java/com/example/Startup.java
+++ src/main/java/com/example/Startup.java
@@ -3,11 +3,6 @@ package com.example;
 import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
-import static java.lang.System.currentTimeMillis;
-
-/**
- * Created by dajudge on 24.08.2016.
- */
 public class Startup {
     public static void main(String argv[]) throws MqttException {
         MqttAsyncClient client = new MqttAsyncClient("ssl://mqtt.exmaple.com", "testClient");
